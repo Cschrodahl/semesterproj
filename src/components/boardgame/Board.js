616c178638ead1f3ba90bf265ea2fee375f111cd
Christian Schrodahl <christian.schrodahl@gmail.com>
2020-04-24 09:37:37 +0200

    commit

--- src/components/boardgame/Board.js
+++ src/components/boardgame/Board.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import Tiles from "./Tiles";
 import RollDice from "./dice/RollDice";
 import Players from "./players/Players";
-let currentPlayerTurn = 1;
+
+//Setting the players turn
+let currentPlayerTurn = 0;
 
 function Board() {
   let addTiles = [];
   const [currentPlayer, setCurrentPlayer] = useState([]);
   const [currentScore, setCurrentPlayerScore] = useState(0);
+  const [currentScore1, setCurrentPlayerScore1] = useState(0);
 
   useEffect(() => {
     const getPlayer = { ...localStorage };
@@ -27,23 +30,32 @@ function Board() {
     }
     setCurrentPlayer(players);
   }, []);
-  console.log(currentPlayer);
   //Creating tiles index
   for (let i = 1; i < 25; i++) {
     addTiles.push(i);
   }
-  const playerTurn = (event, value) => {
-    currentPlayer[currentPlayerTurn].Score += value;
+
+  const playerTurn = (event, rolled) => {
+    currentPlayer[currentPlayerTurn].Score += rolled;
     if (currentPlayer[currentPlayerTurn].Score === 10) {
       currentPlayer[currentPlayerTurn].Score = 1;
       console.log("go back to square 1");
     }
-    setCurrentPlayerScore(currentPlayer[currentPlayerTurn].Score);
+    if (currentPlayerTurn === 0) {
+      setCurrentPlayerScore(currentPlayer[currentPlayerTurn].Score);
+    } else {
+      setCurrentPlayerScore1(currentPlayer[currentPlayerTurn].Score);
+    }
+    //Toggle player turn
     currentPlayerTurn++;
     if (currentPlayerTurn >= 2) {
       currentPlayerTurn = 0;
     }
-
+    console.log(
+      currentPlayer[currentPlayerTurn].player,
+      currentPlayer[currentPlayerTurn].Score
+    );
+    //updating player position
     return (
       <Tiles
         player1={currentPlayer[0].Score}
@@ -69,7 +81,7 @@ function Board() {
                 ) : null
               }
               player2={
-                currentScore === i ? (
+                currentScore1 === i ? (
                   <Players
                     border={currentPlayer[1].image}
                     playerTurn={currentPlayer[1].player}
@@ -81,10 +93,34 @@ function Board() {
           );
         })}
         <div className="board__center">
-          <RollDice value={playerTurn} />
+          <RollDice rolled={playerTurn} />
         </div>
       </div>
     </main>
   );
 }
 export default Board;
+/** <Tiles
+              key={i}
+              index={i}
+              player1={
+                currentScore === i ? (
+                  <Players
+                    border={currentPlayer[0].image}
+                    playerTurn={currentPlayer[0].player}
+                    token={currentPlayer[0].token}
+                  />
+                ) : null
+              }
+              player2={
+                currentScore1 === i ? (
+                  <Players
+                    border={currentPlayer[1].image}
+                    playerTurn={currentPlayer[1].player}
+                    token={currentPlayer[1].token}
+                  />
+                ) : null
+              }
+            />
+          );
+        })} */
--- src/components/boardgame/dice/RollDice.js
+++ src/components/boardgame/dice/RollDice.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import Dice from "./Dice";
-function RollDice({ value }) {
+function RollDice({ rolled }) {
   const [roll, setRoll] = useState(1);
 
   return (
     <div
       onClick={(event) => {
         setRoll(roll - roll + Math.floor(Math.random() * 6) + 1);
-        return value(event, roll);
+        return rolled(event, roll);
       }}
       key={roll}
     >
